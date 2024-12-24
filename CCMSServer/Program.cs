99695fc051347def2a9c46c98725009638635177
human7731 <human7731@naver.com>
2024-12-24 17:55:13 +0900

    -
    -

--- CCMSServer/Program.cs
+++ CCMSServer/Program.cs
@@ -14,11 +14,11 @@ namespace CCMSServer
             // 서버를 실행하기 위한 xml 파일을 읽어옵니다. (CCMSServer/CCMSServer/Configurations 참조)
             ConfigHandler.Instance.ReadAll();
 
-            // 코어 서버를 실행합니다.
-            TCP_CCMSServer.Instance.StartServer();
-
             // DB 연결합니다.
             DBHandler.Instance.Connect();
+
+            // 코어 서버를 실행합니다.
+            TCP_CCMSServer.Instance.StartServer();
 
             // 콘솔로부터 입출력을 받습니다.
             string input = "";
